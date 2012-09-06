09db87d099da3a1f5cc1a74c5f37c6bea33f5cc5
Dan Turner <dst502@york.ac.uk>
2012-09-06 13:55:47 +0100

    Corrected the import in pack/plugins/extending_plugin.py

--- pack/plugins/extending_plugin.py
+++ pack/plugins/extending_plugin.py
@@ -1,4 +1,8 @@
-from pack.Plugin import Plugin
+from pack.plugin import Plugin
 
 class ExtendingPlugin(Plugin):
   pass
+
+if __name__=="__main__":
+  p = ExtendingPlugin()
+  print(p.plugin_name())
